f5f38a2689ea7a1f797532e8e515416b060de75e
Maurice Putz <mauriceputz2000@gmail.com>
2021-01-07 16:22:34 +0100

    +fixing warnings from tabulator library +adding write table in file

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,21 @@
 #include "CLI11.hpp"
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/async.h"
-#include "tabulate/tabulate.hpp"
 #include "rang.hpp"
+
+//ignore warnings from extern library "tabulate"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+#include "tabulate/tabulate.hpp"
+#pragma GCC diagnostic pop
 
 using namespace std;
 using namespace CLI;
 using namespace tabulate;
 
-//const auto logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");  //basic logger
-auto logger = spdlog::basic_logger_mt<spdlog::async_factory>("async_file_logger", "logs/async_log.txt"); //async logger -> faster
+string home = getenv("HOME");
+string logPath = home;
+auto logger = spdlog::basic_logger_mt<spdlog::async_factory>("async_file_logger", logPath.append("/Desktop/mlt3logs/log.txt")); //async logger -> faster
 
 //for encoding the character if the input string to ascii DEC numbers
 short encode(char character_to_encode) {
@@ -31,32 +37,6 @@ short encode(char character_to_encode) {
 
 //returns a random amount from 1 until 127 numbers of random numbers between 33 and 126
 //this are the ASCII character in DEC and all special and alphanumerical character of ASCII
-vector<short> random(string allowed="") {
-    vector<short> res;
-    srand((int)time(0));
-	int repeat = (rand() % (126-33)) + 1;   //how often random ascii sign should be repeated
-
-    if (allowed == "") {
-        int i = 0;
-        while(i++ < repeat) {
-            res.push_back((rand() % (126-33)) + 33); //33 is ! and 126 is ~ all between are numbers, special signs or letters
-        }
-    } else {
-        vector<short> tmp_bitsets;
-        for (size_t i=0; i < allowed.length(); ++i) {
-            tmp_bitsets.push_back(encode(allowed.at(i)));   //get bitset for given ascii signs in allowed
-        }
-
-        int j = 0;
-        while(j++ < repeat) {
-            int random_idx = rand() % tmp_bitsets.size();
-            res.push_back(bitset<8>(tmp_bitsets[random_idx]).to_ullong());    //access random elemnt of the new vector with allowed ascii signs from input
-        }
-    }
-    return res;
-}
-
-//operation overload for table or file output
 vector<short> random_tf(Table& tab, string allowed="") {
     vector<short> res;
     srand((int)time(0));
@@ -151,19 +131,6 @@ string decrypt_from_mlt3(string mlt3_data, Table& tab, int table_col_cnt) {
 
 //sets the DataPromise and Future from sender thread, that the receiver thread
 //can pick up the sended data in binary format
-void send_data(vector<short> data_to_send, Queue<string>& queue) {
-    vector<bitset<8>> help_vec_tmp;
-    for (size_t i=0; i < data_to_send.size(); ++i) {
-        help_vec_tmp.push_back((bitset<8> (data_to_send[i])));
-    }
-    //every bitblog will be converted with mlt3 and send to promise for receiver thread
-    string res = "";
-    for (size_t i=0; i < help_vec_tmp.size(); ++i) {
-        queue.push(convert_to_mlt3(help_vec_tmp[i]));
-    }
-    logger->info("All data succesfulls on queue pushed");
-}
-
 void send_data_tf(vector<short> data_to_send, Queue<string>& queue, Table& tab) {
     vector<bitset<8>> help_vec_tmp;
     for (size_t i=0; i < data_to_send.size(); ++i) {
@@ -195,14 +162,12 @@ int main(int argc, char* argv[]) {
     string input_chars;
 
     bool f = false;
-    bool t = false;
     bool s = false;
     
     App app {"MLT-3 Encoding"};
     app.add_option("input_characters", input_chars, "Only given characters will be random times send over with MLT-3.    Example: \"./mlt3send asdf\"");
-    app.add_flag("-f,--file", f, "Writes the table of the processes into an ASCII-Doc");
-    app.add_flag("-t,--table", t, "Output whole process as table formatted on the command line");
-    app.add_flag("-s,--start", s, "Stop time between input and start of sending until receiving data");
+    app.add_flag("-f,--file", f, "Writes the ouput of MLT-3 process and time measurement into an ASCII-Doc");
+    app.add_flag("-t,--time", s, "Time measurement of sending until receiving data");
 
     //NOTE ADD WHICH ASCII CHARACTERS ARE ALLOWED! 33 until 129 in dec!
     try {
@@ -221,32 +186,43 @@ int main(int argc, char* argv[]) {
     input_chars = string(input_chars.begin(), res);
     //END
 
-    if (f) {
-        AsciiDocExporter exporter;
-        //auto asciidoc = exporter.dump(table);
-
-        //cout << rang::fg::red << "You can render your ASCII Document with an ASCII Rendertool or online at: https://www.tutorialspoint.com/online_asciidoc_editor.php\n" << rang::style::reset;
-        logger->info("write in file succesfull");
-    }
-    if (t) {
-        Table universal_constants;
+    Table main_table;
 
         /*
-        universal_constants.format()
+        main_table.format()
         .font_style({FontStyle::bold})
         .corner_color(Color::magenta)
         .border_color(Color::magenta);
         */
-        universal_constants.add_row({"ASCII data to send in DEC Format", "Binary format", "MTL-3 format", "Binary format encoded from MLT-3 format", "Data recceived in ASCII characters"});
+    main_table.add_row({"ASCII data to send in DEC Format", "Binary format", "MTL-3 format", "Binary format encoded from MLT-3 format", "Data recceived in ASCII characters"});
 
-        thread sender{send_data_tf, random_tf(ref(universal_constants), input_chars), ref(q), ref(universal_constants)};  //ref() for rvalue error in std::thread because its given by reference
-        sender.join();
+    thread sender{send_data_tf, random_tf(ref(main_table), input_chars), ref(q), ref(main_table)};  //ref() for rvalue error in std::thread because its given by reference
+    sender.join();
 
-        thread receiver{decode, ref(q), ref(universal_constants)};
-        receiver.join();
+    thread receiver{decode, ref(q), ref(main_table)};
+    receiver.join();
 
-        cout << universal_constants << "\n";
+    if (!f) {
+        cout << main_table << "\n";
         logger->debug("table printed");
+    }
+    if (f) {
+        AsciiDocExporter exporter;
+        auto asciidoc = exporter.dump(main_table);
+        //auto asciidoc = exporter.dump(table);
+
+        ofstream file;
+        file.open (home.append("/Desktop/mlt3.adoc"));
+        if (!file) {
+            string err_msg = "Could not open file: ";
+            logger->error(err_msg.append(home));
+            cout << "Could not create file at path: " << home << "\n";
+        } else {
+            file << asciidoc;
+            file.close();
+            //cout << rang::fg::red << "You can render your ASCII Document with an ASCII Rendertool or online at: https://www.tutorialspoint.com/online_asciidoc_editor.php\n" << rang::style::reset;
+            logger->info("write in file succesfull");
+        }
     }
 
     if (s) {
